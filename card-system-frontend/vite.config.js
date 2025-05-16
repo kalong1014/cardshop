1983bb217c5f153d7d8f049343a57678d164dd2f
kalong1014 <1096205@qq.com>
2025-05-16 23:06:46 +0800

    Update vite.config.js

--- card-system-frontend/vite.config.js
+++ card-system-frontend/vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import postcss from 'postcss'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-    resolve: {
+  resolve: {
     alias: {
       '@': '/src'
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer
+      ]
     }
   }
 })
